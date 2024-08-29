00b83b56c3b5de8b0916f6b85126940c5348e207
Emmanuel Agbo <link4emmy@gmail.com>
2024-08-29 03:35:43 +0100

    add alx react app new

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -4,6 +4,7 @@ function MainContent(props) {
   return (
     <main style={{ padding: '20px', backgroundColor: '#f9f9f9', margin: '20px auto', maxWidth: '800px', borderRadius: '10px' }}>
       {props.children}
+      <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 }
