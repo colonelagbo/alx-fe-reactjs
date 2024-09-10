89df35871d05b774ad9253562461fbc487685b33
Emmanuel Agbo <link4emmy@gmail.com>
2024-09-10 17:25:40 +0100

    added recipedetail

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,3 +1,4 @@
+// Import necessary modules
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import RecipeDetail from './components/RecipeDetail';
