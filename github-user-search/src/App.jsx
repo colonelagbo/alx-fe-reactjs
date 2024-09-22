263e1160aa8a179061a116130f40bc1f6f753831
Emmanuel Agbo <link4emmy@gmail.com>
2024-09-22 13:32:35 +0100

    added gitub user search

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>GitHub User Search</h1>
+      {/* Add components and routing here */}
+    </div>
+  );
+}
+
+export default App;
